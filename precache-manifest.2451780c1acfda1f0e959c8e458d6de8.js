e55e0cb81df44d7cf878f859205f24b93be1b633
German <germandolia@gmail.com>
2020-06-22 23:01:55 +0300

    Updates

---
+++ precache-manifest.2451780c1acfda1f0e959c8e458d6de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87edeaae3dcdd1a26318df0592b09a35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93ae82d5240ac7df81ac",
+    "url": "./static/css/main.589d19be.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ae82d5240ac7df81ac",
+    "url": "./static/js/main.860d089e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
